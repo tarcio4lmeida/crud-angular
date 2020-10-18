a9c111ad2c06be1ec19cb88bd16990482136630c
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-10-18 16:27:07 -0300

    Product delete

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -24,21 +24,26 @@ export class ProductService {
   }
 
   create (product: Product) :Observable<Product>{
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
 
   read () :Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl);
   }
 
-  readById (id:string) :Observable<Product>{
-    const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+  readById (id: string): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
   }
   
-  update (product: Product) :Observable<Product>{
-    const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+  update (product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  delete (id: string): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 
 }
