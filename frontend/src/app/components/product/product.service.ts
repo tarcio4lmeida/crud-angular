19058c6e16b9a46d6487cfed601f7963f04e4142
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-10-25 23:38:21 -0300

    Cloud URL

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -11,7 +11,7 @@ import { Product } from './product.model';
 
 export class ProductService {
 
-  baseUrl = "http://localhost:8080/products"
+  baseUrl = "https://tarcio-crud-angular.herokuapp.com/products"
 
   constructor(private snackBar: MatSnackBar,
               private http: HttpClient) {}
